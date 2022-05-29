929df97fac0080b908f6421924d55ef3a064e7d2
djnack <dj.nack666@yahoo.com>
2022-05-30 03:17:04 +0430

    v1.0.2

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('link/{slog}', [LinkController::class, 'index'])->name('link_slog');
 
-Route::get('/foo', function () {
+Route::get('/migrate', function () {
     dd(Artisan::call('migrate', []));
 });
